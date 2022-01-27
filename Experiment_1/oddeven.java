f5780a19b6a10e70630bb19cb82e28585177bbc5
VISHAL V NAIR <90333863+vishalvijaynair@users.noreply.github.com>
2022-01-27 14:08:08 +0530

    Add files via upload

---
+++ Experiment_1/oddeven.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class oddeven 
+{
+    public static void main(String args[])
+    {
+    Scanner obj=new Scanner(System.in);
+    System.out.println("Enter a number :");
+    int a=obj.nextInt();
+    if(a%2==0)
+    {
+        System.out.println("The number is even");
+    }
+    else 
+    {
+        System.out.println("The number is odd");
+    }
+    
+}
+}
