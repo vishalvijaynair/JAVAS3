7358237ede4dc84d974e7c6fae21e3c662ae3355
VISHAL V NAIR <90333863+vishalvijaynair@users.noreply.github.com>
2022-01-27 18:39:22 +0530

    Update sum.java

--- Experiment_2/sum.java
+++ Experiment_2/sum.java
@@ -1,1 +1,19 @@
-
+import java.util.Scanner;
+class sum
+{
+    public static void main(String args[])
+    {
+        int n,i,sum=0;
+        Scanner obj=new Scanner(System.in);
+        System.out.print("Enter the limit of the array :");
+        n=obj.nextInt();
+        int a[]=new int[n];
+        System.out.println("Enter the elements in the array");
+        for(i=0;i<n;i++)
+        {
+            a[i]=obj.nextInt();
+            sum=sum+a[i];
+        }
+        System.out.println("The sum of "+n+" elements in the array is "+sum);
+    }
+}
