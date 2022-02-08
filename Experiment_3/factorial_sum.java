777630eb434b965daa616f1f0ed83859551e8923
VISHAL V NAIR <90333863+vishalvijaynair@users.noreply.github.com>
2022-02-08 22:13:09 +0530

    Update factorial_sum.java

--- Experiment_3/factorial_sum.java
+++ Experiment_3/factorial_sum.java
@@ -1,1 +1,30 @@
+import java.util.*;
+public class factorial_sum {
+	public static void main(String args[]) {
+    	factorial_sum obj = new factorial_sum();
+        Scanner ob=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int x=ob.nextInt();
+        ob.close();
+        obj.sum(x);
+        obj.fact(x);
+    }
+    void sum(int x){
+        int s=0,a;
+        while(x>0) {
+     		a=x%10;
+          	s=s+a;
+       		x=x/10;
+    	 }
+        System.out.println("sum :"+s);    
+    }
+     
+    void fact(int n) {
+     	int fat=1,i;
+        for(i=1;i<=n;i++) {
+        	fat=fat*i;
+        }
+        System.out.println("Factorial :"+fat);   
+    }
+} 
 
